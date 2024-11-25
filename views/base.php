3ee9877d028c2fefaa8983ad2a2cddaa52e9585f
Phong NGUYEN <phongnguyen25000@gmail.com>
2024-11-25 22:36:16 +0100

    rectification footer

--- views/base.php
+++ views/base.php
@@ -42,27 +42,19 @@
     <?= $content ?>
 </main>
 
-<footer class="ps-5 pe-5 pt-5 bottom">
-    <div class="text-center">
-        <h5>En savoir plus...</h5>
-        <ul class="nav flex-column">
-            <li class="nav-item mb-1"><a href="#" class="nav-link p-0 text-muted">Accueil</a></li>
-            <li class="nav-item mb-1"><a href="/legal" class="nav-link p-0 text-muted">Mentions légales</a></li>
-            <li class="nav-item mb-1"><a href="#" class="nav-link p-0 text-muted">Offres d'emploi</a></li>
-            <li class="nav-item mb-1"><a href="#" class="nav-link p-0 text-muted">FAQ</a></li>
-            <li class="nav-item mb-1"><a href="#" class="nav-link p-0 text-muted">A propos de nous</a></li>
-        </ul>
-    </div>
-
-    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 mt-3 border-top border-black">
-        <p>© 2024 Groupe Gaudper. Tous droits réservés.</p>
-        <ul class="list-unstyled d-flex">
-            <i class="bi bi-facebook fs-5 me-2"></i>
-            <i class="bi bi-twitter-x fs-5 me-2"></i>
-            <i class="bi bi-tiktok fs-5 me-2"></i>
-            <i class="bi bi-instagram fs-5 me-2"></i>
-            <i class="bi bi-linkedin fs-5"></i>
-        </ul>
+<footer class="fixed-bottom shadow mt-4">
+    <div class="container py-4">
+        <div class="d-flex justify-content-between align-items-center text-sm text-muted">
+            <div>&copy; <?= date('Y') ?> Lycée Gaudper. Tous droits réservés.</div>
+            <div><a href="/legal" class="text-muted">Mentions légales</a></div>
+            <ul class="list-unstyled d-flex">
+                <i class="bi bi-facebook fs-5 me-2"></i>
+                <i class="bi bi-twitter-x fs-5 me-2"></i>
+                <i class="bi bi-tiktok fs-5 me-2"></i>
+                <i class="bi bi-instagram fs-5 me-2"></i>
+                <i class="bi bi-linkedin fs-5"></i>
+            </ul>
+        </div>
     </div>
 </footer>
 </body>
